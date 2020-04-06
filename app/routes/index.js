e35a8117816c7303a7e3e48a3b659ec13b94b6f3
Luisfayre <luisfayre@hotmail.com>
2020-04-05 19:04:05 -0600

    data

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,52 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
     async model() {
-        let response = await fetch('http://api.coronastatistics.live/all');
-        let parsed = await response.json();
-        console.log(parsed.cases);
+        //Catidades totales
+        let all = await fetch('http://api.coronastatistics.live/all');
+        let dataAll = await all.json();
+        // console.log(dataAll);
 
-        return {
-            cases: parsed.cases,
-            deaths: parsed.deaths,
-            recovered: parsed.recovered,
-        };
+        //Cantidades por ciudades
+        let countries = await fetch('http://api.coronastatistics.live/countries');
+        let dataCountries = await countries.json();
+        // console.log(dataCountries);
+
+        //Almacenamos los datos en una variable
+        let alltodayCases = 0;
+        let alltodayDeaths = 0;
+        let alltotalCritical = 0;
+        let allcasesPerOneMillion = 0;
+
+        dataCountries.forEach(dataCountries => {
+            alltodayCases += dataCountries.todayCases;
+            alltodayDeaths += dataCountries.todayDeaths;
+            alltotalCritical += dataCountries.active;
+            allcasesPerOneMillion += dataCountries.casesPerOneMillion;
+        });
+        // console.log(" Hoy: ", alltodayCases, " Muertos: ", alltodayDeaths, " Criticos: ", alltotalCritical, " Pormillon: ", allcasesPerOneMillion)
+
+        return [
+            {
+                title: "Infections",
+                data: dataAll.cases,
+                today: alltodayCases + " today"
+            },
+            {
+                title: "Deaths",
+                data: dataAll.deaths,
+                today: alltodayDeaths + " today"
+            },
+            {
+                title: "Deaths",
+                data: dataAll.recovered,
+                today: alltotalCritical + " remaining"
+            }
+            ,
+            {
+                title: "Critical",
+                data: dataAll.recovered,
+                today: allcasesPerOneMillion + " per million"
+            }];
 
     }
 }
